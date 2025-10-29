032f06ce306fcd8ccf37910ab81b428a3a0fcf29
Tergorz <benjamin@stanzl.com>
2025-10-29 11:18:36 +0100

    testat02 add solution w/ 2 loops

--- Testat02/Testat02.java
+++ Testat02/Testat02.java
@@ -30,16 +30,8 @@ public class Testat02 {
         return groesse;
     }
 
-    public static void main(String[] args) {
-
-        int anzahl = inputSegment();
-        char pattern = inputPattern();
-        int groesse = getGroesse(anzahl);
-
-
-
-
-
+    //Mit 3 Schleifen
+    public static void altSolution(int anzahl, char pattern, int groesse) {
         for (int i = 0; i < anzahl; i++) {
             for( int k = 0; k < groesse; k++) {
                 for (int j = 0; j < (anzahl / 2); j++) {
@@ -75,6 +67,38 @@ public class Testat02 {
                 System.out.print('\n');
             }
         }
+    }
+
+    //Mit 2 Schleifen
+    public static void solution(int anzahl, char pattern, int groesse) {
+        for (int i = 0; i < anzahl; i++) {
+            for (int k = 0; k < groesse; k++) {
+                String zeile = "";
+                int bloecke = anzahl / 2;
+
+                String patternBlock = "" + pattern;
+                patternBlock = patternBlock.repeat(groesse);
+                String nullBlock = "" + '0';
+                nullBlock = nullBlock.repeat(groesse);
+                if (i % 2 == 0) {
+                    zeile = zeile + ((patternBlock + nullBlock).repeat(bloecke));
+                } else {
+                    zeile = zeile + ((nullBlock + patternBlock).repeat(bloecke));
+                }
+                System.out.println(zeile);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int anzahl = inputSegment();
+        char pattern = inputPattern();
+        int groesse = getGroesse(anzahl);
+
+        //altSolution(anzahl, pattern, groesse);
+        solution(anzahl, pattern, groesse);
+
 
 
     }
