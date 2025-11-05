006e64da2f44900ffed2282a56ad8bda9cf78147
Tergorz <benjamin@stanzl.com>
2025-11-05 14:57:27 +0100

    testat02 fix uneven sizes / add rectangle option

--- Testat02/Testat02.java
+++ Testat02/Testat02.java
@@ -1,5 +1,6 @@
 public class Testat02 {
 
+
     public static int inputSegment() {
         System.out.println("Bitte Anzahl der Segmente (Zahl zw. 4 & 12) festlegen. \n Eingabe: ");
         int anzahl = Input.readInt();
@@ -9,6 +10,24 @@ public class Testat02 {
             anzahl = Input.readInt();
         }
         return anzahl;
+    }
+
+    public static int[] inputSegmentOptional() {
+        System.out.println("Bitte Anzahl der vertikalen Segmente (Zahl zw. 4 & 12) festlegen. \n Eingabe: ");
+        int y = Input.readInt();
+        while (y < 3 || y > 13) {
+            if (y < 3) { System.out.println("Zahl zu klein. \n Eingabe: "); }
+            else { System.out.println("Zahl zu groß. \n Eingabe: "); }
+            y = Input.readInt();
+        }
+        System.out.println("Bitte Anzahl der horizontalen Segmente (Zahl zw. 4 & 12) festlegen. \n Eingabe: ");
+        int x = Input.readInt();
+        while (x < 3 || x > 13) {
+            if (x < 3) { System.out.println("Zahl zu klein. \n Eingabe: "); }
+            else { System.out.println("Zahl zu groß. \n Eingabe: "); }
+            x = Input.readInt();
+        }
+        return new int[]{y, x};
     }
 
 
@@ -30,37 +49,65 @@ public class Testat02 {
         return groesse;
     }
 
-    //Mit 3 Schleifen
-    public static void altSolution(int anzahl, char pattern, int groesse) {
+    public static int inputGroesse() {
+        System.out.println("Bitte geben sie die Groesse des Segments ein. (Zahl zw. 1 & 3) \n Eingabe: ");
+        int groesse = Input.readInt();
+        while (groesse < 1 || groesse > 3) {
+            System.out.println("Bitte geben sie eine Zahl zwischen 1 und 3 ein. \n Eingabe: ");
+            groesse = Input.readInt();
+        }
+        return groesse;
+    }
+
+
+    public static void solution(int anzahl, char pattern, int groesse) {
         for (int i = 0; i < anzahl; i++) {
             for( int k = 0; k < groesse; k++) {
-                for (int j = 0; j < (anzahl / 2); j++) {
+                for (int j = 0; j < anzahl; j++) {
                     switch (groesse) {
                         case 1:
                             if(i % 2 == 0) {
-                                System.out.print(pattern);
-                                System.out.print('0');
+                                if(j % 2 == 0) {
+                                    System.out.print(pattern);
+                                } else {
+                                    System.out.print('0');
+                                }
                             } else {
-                                System.out.print('0');
-                                System.out.print(pattern);
+                                if(j % 2 == 0) {
+                                    System.out.print('0');
+                                } else {
+                                    System.out.print(pattern);
+                                }
                             }
                             break;
                         case 2:
                             if(i % 2 == 0) {
-                                System.out.print("" + pattern + pattern);
-                                System.out.print("" + '0' + '0');
+                                if(j % 2 == 0) {
+                                    System.out.print("" + pattern + pattern);
+                                } else {
+                                    System.out.print("" + '0' + '0');
+                                }
                             } else {
-                                System.out.print("" + '0' + '0');
-                                System.out.print("" + pattern + pattern);
+                                if(j % 2 == 0) {
+                                    System.out.print("" + '0' + '0');
+                                } else {
+                                    System.out.print("" + pattern + pattern);
+                                }
                             }
                             break;
                         case 3:
                             if(i % 2 == 0) {
-                                System.out.print("" + pattern + pattern + pattern);
-                                System.out.print("" + '0' + '0' + '0');
+                                if(j % 2 == 0) {
+                                    System.out.print("" + pattern + pattern + pattern);
+                                } else {
+                                    System.out.print("" + '0' + '0' + '0');
+                                }
                             } else {
-                                System.out.print("" + '0' + '0' + '0');
-                                System.out.print("" + pattern + pattern + pattern);
+                                if(j % 2 == 0) {
+                                    System.out.print("" + '0' + '0' + '0');
+                                } else {
+                                    System.out.print("" + pattern + pattern + pattern);
+                                }
                             }
                     }
                 }
@@ -69,35 +116,110 @@ public class Testat02 {
         }
     }
 
-    //Mit 2 Schleifen
-    public static void solution(int anzahl, char pattern, int groesse) {
-        for (int i = 0; i < anzahl; i++) {
-            for (int k = 0; k < groesse; k++) {
-                String zeile = "";
-                int bloecke = anzahl / 2;
-
-                String patternBlock = "" + pattern;
-                patternBlock = patternBlock.repeat(groesse);
-                String nullBlock = "" + '0';
-                nullBlock = nullBlock.repeat(groesse);
-                if (i % 2 == 0) {
-                    zeile = zeile + ((patternBlock + nullBlock).repeat(bloecke));
-                } else {
-                    zeile = zeile + ((nullBlock + patternBlock).repeat(bloecke));
+    public static void solutionOptional(int y, int x, char pattern, int groesse) {
+        for (int i = 0; i < y; i++) {
+            for( int k = 0; k < groesse; k++) {
+                for (int j = 0; j < x ; j++) {
+                    switch (groesse) {
+                        case 1:
+                            if(i % 2 == 0) {
+                                if(j % 2 == 0) {
+                                    System.out.print(pattern);
+                                } else {
+                                    System.out.print('0');
+                                }
+                            } else {
+                                if(j % 2 == 0) {
+                                    System.out.print('0');
+                                } else {
+                                    System.out.print(pattern);
+                                }
+                            }
+                            break;
+                        case 2:
+                            if(i % 2 == 0) {
+                                if(j % 2 == 0) {
+                                    System.out.print("" + pattern + pattern);
+                                } else {
+                                    System.out.print("" + '0' + '0');
+                                }
+                            } else {
+                                if(j % 2 == 0) {
+                                    System.out.print("" + '0' + '0');
+                                } else {
+                                    System.out.print("" + pattern + pattern);
+                                }
+                            }
+                            break;
+                        case 3:
+                            if(i % 2 == 0) {
+                                if(j % 2 == 0) {
+                                    System.out.print("" + pattern + pattern + pattern);
+                                } else {
+                                    System.out.print("" + '0' + '0' + '0');
+                                }
+                            } else {
+                                if(j % 2 == 0) {
+                                    System.out.print("" + '0' + '0' + '0');
+                                } else {
+                                    System.out.print("" + pattern + pattern + pattern);
+                                }
+                            }
+                    }
                 }
-                System.out.println(zeile);
+                System.out.print('\n');
             }
         }
     }
 
+
+
     public static void main(String[] args) {
 
-        int anzahl = inputSegment();
-        char pattern = inputPattern();
-        int groesse = getGroesse(anzahl);
+        System.out.println("Sollen Rechtecke erlaubt werden? (y/n)");
+        char option = Character.toLowerCase(Input.readChar());
+        //Rechteckiges Feld
+        if (option == 'y') {
+            int[] size = inputSegmentOptional();
+            int y = size[0];
+            int x = size[1];
+            char pattern = inputPattern();
+            int groesse = inputGroesse();
 
-        //altSolution(anzahl, pattern, groesse);
-        solution(anzahl, pattern, groesse);
+            solutionOptional(y, x, pattern, groesse);
+
+            System.out.println("Möchten sie noch ein weiteres Muster entwerfen? (y/n)");
+            char repeat = Character.toLowerCase(Input.readChar());
+            while (repeat == 'y') {
+                size = inputSegmentOptional();
+                pattern = inputPattern();
+                groesse = inputGroesse();
+                solutionOptional(y, x, pattern, groesse);
+                System.out.println("Möchten sie noch ein weiteres Muster entwerfen? (y/n)");
+                repeat = Character.toLowerCase(Input.readChar());
+            }
+        }
+        //Quadratisches Feld
+        else {
+            int anzahl = inputSegment();
+            char pattern = inputPattern();
+            int groesse = getGroesse(anzahl);
+
+            solution(anzahl, pattern, groesse);
+
+            System.out.println("Möchten sie noch ein weiteres Muster entwerfen? (y/n)");
+            char repeat = Character.toLowerCase(Input.readChar());
+            while (repeat == 'y') {
+                anzahl = inputSegment();
+                pattern = inputPattern();
+                groesse = getGroesse(anzahl);
+                solution(anzahl, pattern, groesse);
+                System.out.println("Möchten sie noch ein weiteres Muster entwerfen? (y/n)");
+                repeat = Character.toLowerCase(Input.readChar());
+            }
+        }
+
+
 
 
 
